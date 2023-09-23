c8b1abbb9eac3214f73f5e37ff83e2144c493ef0
Kaif Momin <kaifmomin30@gmail.com>
2023-09-23 17:15:35 -0400

    tried to do login, getting some issues, need second pov

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   console.log("APP Running");
-  return <WelcomeScreen />;
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="WelcomeScreen">
+        <Stack.Screen
+          name="WelcomeScreen"
+          component={WelcomeScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,46 +1,118 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  SafeAreaView,
+} from "react-native";
 import { useFonts } from "expo-font";
+import React, { useState, useEffect } from "react";
+import { FIREBASE_AUTH } from "../../firebaseConfig";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 
-function WelcomeScreen(props) {
+const WelcomeScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const auth = FIREBASE_AUTH;
+
   const [fontsLoaded] = useFonts({
-    Questrial: require("../assets/font/Questrial-Regular.ttf"),
+    Red_Hat_Display: require("../assets/font/Red_Hat_Display/RedHatDisplay-Light.ttf"),
   });
 
+  useEffect(() => {
+    if (!fontsLoaded) {
+      return;
+    }
+  }, [fontsLoaded]);
+
+  const login = async () => {
+    setLoading(true);
+    try {
+      const response = await signInWithEmailAndPassword(auth, email, password);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+      alert("Login failed: " + error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const signUp = async () => {
+    setLoading(true);
+    try {
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(response);
+      alert("Check you email!");
+    } catch (error) {
+      console.log(error);
+      alert("Sign up failed: " + error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.primaryText}>welcome to {"\n"} shout</Text>
-      <Text style={styles.primaryText}>
-        Working on implementing Login/Signup page
-      </Text>
-      <View style={styles.button}>
-        <Text style={styles.secondaryText}>Login</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.primaryText}>welcome to {"\n"}shout!</Text>
+      <View>
+        <TextInput
+          value={email}
+          style={styles.input}
+          placeholder="email"
+          autoCapitalize="none"
+          onChangeText={(text) => setEmail(text)}
+        ></TextInput>
+        <TextInput
+          secureTextEntry={true}
+          value={password}
+          style={styles.input}
+          placeholder="password"
+          autoCapitalize="none"
+          onChange={(text) => setPassword(text)}
+        ></TextInput>
+
+        {loading ? (
+          <ActivityIndicator size="large" color="red" style={styles.input} />
+        ) : (
+          <>
+            <Button title="Login" onPress={() => login()} />
+            <Button title="Sign Up" onPress={() => signUp()} />
+          </>
+        )}
       </View>
-      <View style={styles.button}>
-        <Text style={styles.secondaryText}>Signup</Text>
-      </View>
-    </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "black",
-    alignItems: "center",
-    justifyContent: "center",
-    fontFamily: "Questrail",
+    fontFamily: "Red_Hat_Display",
   },
   primaryText: {
-    //fontFamily: "Questrial",
-    fontSize: 20,
-    color: "#18a1e7",
-    justifyContent: "center",
-    alignItems: "center",
+    fontSize: 50,
+    color: "#fff",
   },
-  secondaryText: {
-    color: "black",
-    justifyContent: "center",
-    alignItems: "center",
+  input: {
+    marginVertical: 4,
+    height: 50,
+    width: "50%",
+    borderRadius: 15,
+    padding: 10,
+    backgroundColor: "#484C57",
+    alignContent: "center",
   },
   button: {
     width: "25%",
@@ -48,7 +120,7 @@ const styles = StyleSheet.create({
     margin: 10,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#18a1e7",
+    backgroundColor: "#484C57",
   },
 });
 
